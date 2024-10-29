35bc321e3c283ad2f4307bfb926e2023693a717e
Anna-SA2002 <anfumian8982@gmail.com>
2024-10-29 22:34:26 +0900

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
 
+// 共通部分でナビとフッターの呼び出し
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+    <div className="App">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
