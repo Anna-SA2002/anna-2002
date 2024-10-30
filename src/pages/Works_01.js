042eb2ac2ec87c4a905aad1490cc539b3b93f56b
Anna-SA2002 <anfumian8982@gmail.com>
2024-10-30 14:13:12 +0900

    1030

--- src/pages/Works_01.js
+++ src/pages/Works_01.js
@@ -20,7 +20,6 @@ export default function WORKS() {
         </ul>
       </div>
 
-      ////960×720 に画像をトリミングする
       <div className={styles.works_content_illust}>
         <ul className={styles.works_list}>
           <li>
